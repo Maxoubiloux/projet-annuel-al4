4864941295ead5a283f425fd1207b26453c23326
Mirage <cyprien.2003@icloud.com>
2026-07-30 10:59:14 +0200

    fix images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || undefined;
 const nextConfig: NextConfig = {
   basePath,
   images: {
+    unoptimized: Boolean(basePath),
     remotePatterns: [
       {
         protocol: "https",
